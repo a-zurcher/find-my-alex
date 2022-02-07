045599281e5b6dd3b443d520148deb44e6b542b8
Alex <52360674+a-zurcher@users.noreply.github.com>
2022-02-07 13:20:16 +0100

    Update stores.ts

--- src/stores.ts
+++ src/stores.ts
@@ -1,10 +1,9 @@
 import { writable } from "svelte/store";
 
+export const authentificated = writable(false);
+
 // get token from localStorage
 export const authToken = writable(localStorage.getItem("authToken"));
 
 // saves token to localStorage
 authToken.subscribe(value => localStorage.setItem("authToken", value));
-
-
-export const authentificated = writable(false);
