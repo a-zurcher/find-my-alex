8054b587ebe0181d555b6857683c04b22bb9e1bb
Alexandre Zürcher <alexandre.zurcher@ik.me>
2022-09-29 16:25:27 +0200

    removed moved files

--- src/stores.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store";
-
-export const authentificated = writable(false);
-
-// get token from localStorage
-export const authToken = writable(localStorage.getItem("authToken"));
-
-// saves token to localStorage
-authToken.subscribe(value => localStorage.setItem("authToken", value));
